5a204821b370bc24da192d066866cbc3a2f3742b
Adrian Price-Whelan <adrian.prw@gmail.com>
2012-10-12 15:55:07 -0400

    add uncertainty suggestion to quantity API

--- quantity_api.py
+++ quantity_api.py
@@ -18,12 +18,24 @@ to do operations between other quantity classes to convert the units automatical
 
 # The preferred method for creating a quantity is through operations with units
 q = 11.42 * u.meter # returns a Quantity object
+q = 11.42 / u.meter
 q = 182.234 + u.meter # raises an error
 
 # You could also use the Quantity constructor, but units must be supplied
 q1 = u.Quantity(11.412, unit=u.meter)
-q2 = u.Quantity(21.52, unit="cm")
+q2 = u.Quantity(21.52, "cm")
 q3 = u.Quantity(11.412) # raises an exception because no unit specified
+
+# A Quantity object may also have an uncertainty associated with them. I propose, 
+#   at least initially, to allow any object in here. This gives the user the option
+#   to carry along a single value, a probability distribution, unequal error bars, etc.
+#   In the future, we can implement various propagation schemes if the user passes in
+#   a VarianceUncertainty or StandardDeviationUncertainty, for example.
+q1 = u.Quantity(11.41, u.meter, uncertainty=0.01)
+q2 = u.Quantity(14.6, u.meter, uncertainty=0.2)
+q1 = u.Quantity(14.6, u.meter, uncertainty=some_probability_distribution)
+q2 = u.Quantity(14.6, u.meter, uncertainty=(0.1, 0.2))
+# etc..
 
 # Operations
 # ----------
@@ -41,6 +53,10 @@ new_quantity = quantity1 - quantity2
 new_quantity = quantity1 * quantity2
 new_quantity = quantity1 / quantity2
 new_quantity = -quantity1
+
+# At least initially, the resultant quantity object would have uncertainty = None. 
+new_quantity = quantity1 + quantity2 # *drop the uncertainties from the resulting object*
+# new_quantity.uncertainty = None
 
 # Converting units
 # ----------------
