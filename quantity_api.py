faca7a70cb920d94cd604c747f52e0bcbbf53613
Adrian Price-Whelan <adrian.prw@gmail.com>
2012-10-16 23:33:36 -0400

    adopt latest feedback, table uncertainties for future discussion

--- quantity_api.py
+++ quantity_api.py
@@ -5,10 +5,6 @@ from __future__ import print_function
 
 import astropy.units as u
 
-# ============
-# Base Objects
-# ============
-
 """ The Quantity class will represent a number + unit + uncertainty """
 
 # -------------------
@@ -25,17 +21,16 @@ q1 = u.Quantity(11.412, unit=u.meter)
 q2 = u.Quantity(21.52, "cm")
 q3 = u.Quantity(11.412) # raises an exception because no unit specified
 
-# A Quantity object may also have an uncertainty associated with them.
-#   We accept any object in the uncertainty keyword, which enables the user to specify
-#   their own custom objects, full probability distributions, uneven error bars, etc.
+# A Quantity object may also have an uncertainty associated with them, but this will be 
+#   implemented at a later date, probably in a subclass of Quantity. The usage might look
+#   something like this:
 q1 = u.Quantity(14.6, u.meter, uncertainty=some_probability_distribution)
 q2 = u.Quantity(14.6, u.meter, uncertainty=(0.1, 0.2))
 q1 = u.Quantity(11.41, u.meter, uncertainty=0.01) 
 q2 = u.Quantity(14.6, u.meter, uncertainty=0.2)
-# ** For any of the above cases, performing any operation on the object will *set the resulting uncertainty to None* **
 
 # We will also support a number of astropy Uncertainty classes, but the names have not been
-#   decided upon. That will look something like:
+#   decided upon. That will look like:
 q1 = u.Quantity(14.6, u.meter, uncertainty=StandardDeviationUncertainty(0.5))
 q2 = u.Quantity(14.6, u.meter, uncertainty=VarianceUncertainty(0.25))
 # etc.
@@ -44,7 +39,7 @@ q2 = u.Quantity(14.6, u.meter, uncertainty=VarianceUncertainty(0.25))
 # Operations
 # ----------
 
-# Each of these operations would return a new Quantity object *if and only if the units match*
+# Each of these operations would return a new Quantity object
 q1 = u.Quantity(11.41, u.meter)
 q2 = u.Quantity(15.37, u.meter)
 new_quantity = q1 + q2
@@ -54,10 +49,15 @@ new_quantity = q1 / q2
 new_quantity = -q1
 
 # Operations will *adopt the units from the object on the left* if the units are equivalent
-#   for both objects involved:
+#   for both objects involved, and may raise an exception otherwise:
 q1 = u.Quantity(11.41, u.meter)
 q2 = u.Quantity(15.37, u.kilometer)
 new_quantity = q1 + q2 # has unit u.meter!
+
+q1 = u.Quantity(11.41, u.meter)
+q2 = u.Quantity(15.37, u.second)
+q1 / q2 # valid, returns an object in meters per second
+q1 + q2 # raises an exception
 
 # For objects with uncertainties, we can't propagate errors unless the user uses one of the astropy
 #   Uncertainty classes, which know how to do their own error propagation (but are still in development).
@@ -66,12 +66,6 @@ new_quantity = q1 + q2 # has unit u.meter!
 q1 = u.Quantity(11.41, u.meter, uncertainty=StandardDeviationUncertainty(0.35))
 q2 = u.Quantity(15.37, u.meter, uncertainty=StandardDeviationUncertainty(0.11))
 new_quantity = q1 + q2 # error propagation done for the user
-
-# For any ambiguous combinations, we *drop the uncertainties*
-q1 = u.Quantity(11.41, u.meter, uncertainty=0.01) 
-q2 = u.Quantity(14.6, u.meter, uncertainty=0.2)
-new_quantity = q1 + q2 # drop the uncertainties from the resulting object
-# e.g. new_quantity.uncertainty = None
 
 # The Units package will have a native type that represents "no unit." Operations with such dimensionless
 #   objects will look like this (replace unit="" with whatever is decided for Unit package):
@@ -90,15 +84,17 @@ q = 11.412*u.meter
 q.to(u.kilometer) # this returns a new quantity object with the supplied units
 q.to(u.zettastokes) # Fails because not equivalent
 
-# you may also just want the *value* of the quantity in some units. To do this, use the value_in() method:
-quantity1.value_in(u.centimeter) # returns the *value* (just a number, not an object) of this quantity in the specified units
+# you may also just want the *value* of the quantity in some units. To do this, use the value attribute:
+q1 = Quantity(194.118, unit=u.kilometer)
+q1.value # returns 194.118
+q1.to(u.centimeter).value # returns the *value* (just a number, not an object) of this quantity in the specified units
 
 # --------
 # Equality
 # --------
-Quantity(1000, units='m') == Quantity(1, units='km') # returns True
-Quantity(1, units='m') == Quantity(1, units='km') # returns False
-Quantity(1, units='m') == Quantity(1, units='s') # raises IncompatibleUnitError()
+Quantity(1000, unit=u.m) == Quantity(1, units=u.km) # returns True
+Quantity(1, units=u.m) == Quantity(1, units=u.km) # returns False
+Quantity(1, units=u.m) == Quantity(1, units=u.s) # raises IncompatibleUnitError()
 
 # ----------
 # Displaying
