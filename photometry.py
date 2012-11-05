59e07908751e5529f53d07298520153bd2cde308
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-11-05 13:50:13 +0100

    More improvements to API document

--- photometry.py
+++ photometry.py
@@ -1,25 +1,77 @@
+# About
+# =====
+#
+# Current photometry functionality is being developed in the photutils
+# affiliated package, with the aim of ultimately merging into
+# astropy.photometry, so the present API document presents the final
+# user-level API once the code is merged into the core package. In
+# addition, the present document gives no indications of how we deal
+# with things under the hood - we are only concerned with the user-level
+# API.
+#
+# Authors
+# =======
+#
+# * Thomas Robitaille
+#
+# Requirements
+# ============
+#
+# The main requirements of the API are:
+#
+# * Provide an interface for users to programmatically perform aperture and
+#   PSF photometry.
+#
+# * Allow users to easily define custom apertures and PSFs for use with the
+#   photometry routines.
+#
+# Usage
+# =====
+#
 # Regions
 # -------
 
-# PSF
-# ---
+# Apertures
+# ---------
+#
+# Apertures are objects that have a speciifc geometry, and are assumed
+# to be centered on the origin.
 
+from astropy.photometry import CircularAperture
+a1 = CircularAperture(1.)
+
+from astropy.photometry import EllipticalAnnulusAperture
+a2 = EllipticalAnnulusAperture(1.5, 2.0, 1.0, np.pi / 3.)
+
+# The Aperture class can be sub-classed for user-defined shapes:
+
+p3 = CustomAperture()
+
+# The details of the attributes to set and methods to overload is not
+# described here, since it is unimportant for the user.
+#
+# PSFs
+# ----
+#
+# PSFs are 2-d maps of source shapes, and can be defined either
+# analytically or numerically on a grid. Some common PSF shapes are
+# provided for convenience.
+#
 # Define a gaussian PSF with sigma=2.
 
-from photutils.psf import GaussianPSF
-
+from astropy.photometry import GaussianPSF
 p1 = GaussianPSF(sigma=2.)
 
 # Define a gaussian with an arbitrary function:
 
-from photutils.psf import PSF
+from astropy.photometry import PSF
 
 def my_custom_psf(x):
     return np.sin(x) / x
 
 p2 = PSF(my_custom_psf)
 
-# ... or as a lambda function:
+# which, in this case, can also be written as a lambda function:
 
 p2 = PSF(lambda x: np.sin(x) / x)
 
@@ -32,13 +84,17 @@ p3 = SpitzerPSF('irac_8.0_psf.fits')
 
 # The details of the attributes to set and methods to overload is not
 # described here, since it is unimportant for the user.
-
-# PSF Photometry
-# --------------
-
-# To perform the actual photoemtry, the simplest is to pass a Numpy
-# array with pixel coordinates at which to do the photometry, as well as
-# the PSF itself:
+#
+# Photometry
+# ----------
+#
+# We need to ensure that the API for aperture and PSF photometry is as
+# similar as possible to make it easier for users. The basic API for the
+# aperture and PSF photometry is therefore of the form:
+#
+# results = <type>_photometry(<data>, <coordinates>, <aperture or PSF>)
+#
+# The simplest case is to do photometry on a 2-d Numpy array using pixel coordinates:
 
 from astropy.io import fits
 image = fits.getdata('image.fits')
@@ -46,18 +102,26 @@ image = fits.getdata('image.fits')
 x = np.array([1., 4., 2., 7.])
 y = np.array([3., 7., 2., 3.])
 
-from photutils.psf import GaussianPSF
+from astropy.photometry import GaussianPSF
 psf = GaussianPSF(sigma=2.)
 
-from photutils.psf import psf_photometry
-results = psf_photometry(image, (x, y), psf)
+from astropy.photometry import psf_photometry
+results_psf = psf_photometry(image, (x, y), psf)
+
+from astropy.photometry import CircularAperture
+ap = CircularAperture(radius=3.)
+
+from astropy.photometry import aperture_photometry
+results_ap = aperture_photometry(image, (x, y), ap)
 
 # The object returned is an astropy.table.Table object, which for all
 # intents and purposes, behaves like a Numpy structured array, but also
 # includes meta-data and allows easy I/O. The rationale for using a
 # structured-like array is that any number of fields can be returned,
 # and new fields can be added in future without breaking backward
-# compatibility.
+# compatibility. The rationale for using an astropy.table.Table object
+# is because it adds table-manipulation methods that users normally
+# expect of structured arrays, but which structured arrays do not have.
 
 # Make a plot of the flux vs error
 
@@ -78,7 +142,7 @@ results = psf_photometry(data, galactic, lambda x: np.sin(x) / x)
 
 # Integration with NDData
 # -----------------------
-
+#
 # While the photometry can be performed on data in the form of a Numpy
 # array, one can also pass an astropy.nddata.NDData object (or
 # anything initializing an NDData object) with uncertainties:
@@ -90,20 +154,21 @@ results = psf_photometry(data, (x, y), psf)
 # which allows masks and uncertainties to be set on the NDData object
 # (the behavior of the mask when doing photometry will need to be well
 # documented) Not all types of uncertainties will be supported by
-# psf_photometry, so if a type of uncertainty is passed which is not
-# supported, an exception will be raised:
+# psf_photometry and aperture_photometry, so if a type of uncertainty is
+# passed which is not supported, an exception will be raised:
 
 data = NDData(image, uncertainty=CustomUncertainty(unc_image))
 results = psf_photometry(data, (x, y), psf)
 
 # UnsupportedUncertaintyError: cannot perform PSF photometry on data
 # with uncertainties of type CustomUncertainty.
-
+#
 # Once photutils is integrated into astropy.photometry, one could
-# envisage also allowing this function to be accessible as a method on
+# envisage also allowing these function to be accessible as a method on
 # NDData, with exactly the same behavior:
 
-results = image.psf_photometry((x, y), psf)
+results_psf = image.psf_photometry((x, y), psf)
+results_ap = image.aperture_photometry((x, y), ap)
 
 # Integration with astropy.wcs and astropy.coordinates
 # ----------------------------------------------------
@@ -117,15 +182,12 @@ results = psf_photometry(data, galactic, psf)
 # The behavior of the rotation of the PSF will need to be well
 # documented in cases where non-pixel coordinates are used, and the
 # PSF is non-axisymmetric.
-
+#
 # Syntax
 # ------
-
-# The arguments will be named arguments so that code can more easily
-# understandable:
+#
+# Wherever possible, arguments will be named arguments so that code
+# can more easily understandable, e.g.:
 
 results = psf_photometry(data=image, coords=galactic,
                          psf=lambda x: np.sin(x) / x)
-
-# but this can only be done once photutils is part of the core astropy
-# package.
