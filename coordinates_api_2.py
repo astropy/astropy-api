7272fbf913007e593842da862a5cff21e4485e4a
Erik Tollerud <erik.tollerud@gmail.com>
2013-12-31 02:57:56 -0600

    Low-level API work

--- coordinates_api_2.py
+++ coordinates_api_2.py
@@ -23,7 +23,7 @@ c2 = c.SphericalRepresentation(lat=[5, 6]*u.deg, lon=[8, 9]*u.hour, distance=10*
 assert len(c2.distance) == 2
 
 c2 = c.SphericalRepresentation('5:10:20.52 +23:23:23.5', units=(u.hourangle, u.degree))
-# In the current API, `unit` is confusing because sometimes it's one object and sometimes two (issue #1421).  
+# In the current API, `unit` is confusing because sometimes it's one object and sometimes two (issue #1421).
 # But because this is the *only* time `units` is meaningful, it's ok here
 assert c2.lon.units == u.hourangle
 
@@ -73,7 +73,56 @@ with raises(UnitsError):
 c.CartesianRepresentation(x=randn(100), y=randn(100), z=randn(100), units=u.kpc)
 #end OPTION
 
-#OPTION/extension: add more representations like cylindrical or elliptical
+#Future extensions: add more representations like cylindrical or elliptical
 
-#---------------"Low-level" classes"
-c.ICRS
+
+
+#<---------------------"Low-level"/Reference Frame classes--------------------->
+#The low-level classes have a dual role: they act as specifiers of coordinate
+#frames and they *may* also contain representations, in which case they are the
+#actual coordinate objects themselves.
+
+
+#They can always accept a representation as a first argument
+icrs = c.ICRS(c.SphericalRepresentation(lat=5*u.deg, lon=8*u.hour))
+
+
+#Frames that require additional data like equinoxs or obstimes get them as
+#keyword parameters to the frame constructor.  Where sensible, defaults are used
+fk5 = c.FK5(c.SphericalRepresentation(lat=5*u.deg, lon=8*u.hour))  # FK5 is almost always J2000 equinox
+J2000 = astropy.time.Time('J2000',scale='utc')
+fk5_2000 = c.FK5(c.SphericalRepresentation(lat=5*u.deg, lon=8*u.hour), equinox=J2000)
+assert fk5.equinox == fk5_2000.equionx
+
+#the frame data are immutible
+J2001 = astropy.time.Time('B1950',scale='utc')
+fk5.equinox = J2001  # raises AttributeError
+
+
+#If no data or None is given, the class acts as a specifier of a frame, but
+#without any stored data.  These frames are primarily useful for specifying
+#what a coordinate should be trasnformed *into*:
+
+newfk5 = fk5.transform_to(c.FK5(equinox=J2001))  # precesses the data point to the new equinox
+assert newfk5.equinox == J2001
+
+#If there is data, it can be accessed through representation objects
+assert icrs.represent_as(c.SphericalRepresentation).lat == 5*u.deg
+assert icrs.spherical.lat == 5*u.deg  # shorthand for the above
+assert icrs.cartesian.z.value > 0
+
+#Most frames have a "preferred" representation, usually the one in which they are
+#typically shown, often with a special name for some of the coordinates. They can
+#be accessed this way as a equivalent shorthand for the representation form
+assert icrs.ra == 5*u.deg
+assert fk5.dec == 8*u.hour
+
+#the frames can also be initialized with the "preferred" names:
+icrs_2 = c.ICRS(ra=8*u.hour, dec=5*u.deg, distance=1*u.kpc)
+assert icrs == icrs2
+fk5_2 = c.FK5(ra=8*u.hour, dec=5*u.deg, distance=1*u.kpc, equinox=J2000)
+
+
+#the frames also know how to give a reasonable-looking string of themselves,
+#based on the preferred coordinate system
+assert str(icrs_2) == '<ICRS RA=120.000 deg, Dec=5.00000 deg, Distance=1 kpc>'
