c7e6fa7b5c3fdae4a7ae06eda20de7d2cb088ea7
Erik Tollerud <erik.tollerud@gmail.com>
2014-01-20 03:04:52 -0500

    Through high-levels

--- coordinates_api_2.py
+++ coordinates_api_2.py
@@ -95,41 +95,99 @@ fk5_2000 = c.FK5(c.SphericalRepresentation(lat=5*u.deg, lon=8*u.hour), equinox=J
 assert fk5.equinox == fk5_2000.equionx
 
 #the information required to specify the frame is immutible
-J2001 = astropy.time.Time('B1950',scale='utc')
 fk5.equinox = J2001  # raises AttributeError
 
-#OPTION: the *data* might not be immutible:
+#OPTION: the *representation data* might not be immutible:
 fk5.data = c.SphericalRepresentation(lat=6*u.deg, lon=9*u.hour)
 #OR, it might be immutible:
 fk5.data = ... #raises AttributeError
 #end OPTION
 
-#If there is data, it can be accessed through representation objects
+#There is also a class-level attribute that lists the attributes needed to
+#identify the frame.  These include attributes like the `equinox` above.
+assert fk5.framespecattrs == ('equinox', 'obstime')
+assert FK5.framespecattrs == ('equinox', 'obstime')  # defined on the *class*
+
+
+#The actual position information is accessed via the representation objects
 assert icrs.represent_as(c.SphericalRepresentation).lat == 5*u.deg
 assert icrs.spherical.lat == 5*u.deg  # shorthand for the above
 assert icrs.cartesian.z.value > 0
 
-#Most frames have a "preferred" representation, usually the one in which they
-#are typically shown, often with a special name for some of the coordinates.
-#They can be accessed this way as a shorthand for accessing the representations
+#Many frames have a "preferred" representation, the one in which they are
+#conventionally described, often with a special name for some of the
+#coordinates. E.g., most equatorial coordinate systems are spherical with RA and
+#Dec. This works simply as a shorthand for the longer form above
 
 assert icrs.ra == 5*u.deg
 assert fk5.dec == 8*u.hour
 
+assert icrs.preferred_representation == c.SphericalRepresentation
+
 #the frames can also be initialized with the preferred names:
 icrs_2 = c.ICRS(ra=8*u.hour, dec=5*u.deg, distance=1*u.kpc)
 assert icrs == icrs2
-fk5_2 = c.FK5(ra=8*u.hour, dec=5*u.deg, distance=1*u.kpc, equinox=J2000)
 
 
 #the frames also know how to give a reasonable-looking string of themselves,
-#based on the preferred coordinate system and
+#based on the preferred representation and possibly distance
 assert str(icrs_2) == '<ICRS RA=120.000 deg, Dec=5.00000 deg, Distance=1 kpc>'
 
 
+#<-------------------------Transformations------------------------------------->
+#Transformation transform low-level classes from one frame to another.
 #If no data (or `None`) is given, the class acts as a specifier of a frame, but
-#without any stored data.  These frames are primarily useful for specifying
-#what a coordinate should be trasnformed *into*:
+#without any stored data.
+J2001 = astropy.time.Time('J2001',scale='utc')
+fk5_J2001_frame = c.FK5(equinox=J2001)
 
-newfk5 = fk5.transform_to(c.FK5(equinox=J2001))  # precesses the data point to the new equinox
+#if they do not have data, the string instead is the frame specification
+assert str(fk5_J2001_frame) == "<FK5 frame: equinox='J2000.000', obstime='B1950.000'>"
+
+#These frames are primarily useful for specifying what a coordinate should be
+# transformed *into*, as they are used by the `transform_to` method
+
+newfk5 = fk5.transform_to(fk5_J2001_frame)  # precesses the point to the new equinox
 assert newfk5.equinox == J2001
+
+#classes can also be given to `transform_to`, which then uses the defaults for
+#the frame information:
+samefk5 = fk5.transform_to(c.FK5)
+#`fk5` was initialized using default `obstime` and `equinox`, so:
+assert samefk5.ra == fk5.ra and samefk5.dec == fk5.dec
+
+
+#transforming to a new frame necessarily loses framespec information if it
+#is not necessary for the new frame, so transforms are not necessarily
+#round-trippable unless the frame is explicitly given:
+fk5_2 =c.FK5(ra=8*u.hour, dec=5*u.deg, equinox=J2001)
+ic_trans = fk5_2.transform_to(c.ICRS)
+fk5_trans = fk5_2.transform_to(c.FK5)
+assert fk5_2.ra == fk5_trans.ra  # AssertionError - fk5_trans is in the J2000
+# equinox instead of J2001, so it does not have the same RA/Dec
+fk5_trans_2 = fk5_2.transform_to(fk5_2001_frame)
+assert fk5_2.ra == fk5_trans_2.ra  # Now all is fine because same equinox
+
+#Trying to tansforming a frame with no data is of course an error:
+c.FK5(equinox=J2001).transform_to(c.ICRS)  # ValueError
+
+#To actually define a new transformation, the same basic scheme as in the
+#0.2/0.3 coordinates framework is used - a graph of transform functions
+#connecting various coordinate classes together.  The main changes are:
+# 1) The transform functions now get the frame object they are trasnforming the
+#    current data into.
+# 2) Frames with additional information need to have a way to transform between
+#    objects of the same class, but with different framespecinfo values
+
+#An example transform function:
+@c.dynamic_transform_matrix(SomeNewSystem, FK5)
+def new_to_fk5(newobj, fk5frame):
+    ot = newobj.obstime
+    eq = fk5frame.equinox
+    # ... build a *cartesian* transform matrix using `eq` that trasnforms from
+    # the `newobj` frame as observed at `ot` to FK5 an equinox `eq`
+    return matrix
+
+
+
+#<---------------------------"High-level" class-------------------------------->
